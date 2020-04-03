0784d2ee88038861a085eb13b36359160821a4c6
kth496 <taehongkim496@gmail.com>
2020-04-04 01:42:37 +0900

    init

---
+++ include/lab1_sched_types.h
@@ -0,0 +1,14 @@
+#ifndef LAB1_SCHED
+#define LAB1_SCHED
+
+class sched_entity {
+      private:
+        int arrival_time;
+
+      public:
+        sched_entity(/* args */);
+};
+
+sched_entity::sched_entity() {}
+
+#endif
---
+++ lab1_sched.cpp
@@ -0,0 +1,4 @@
+#include "include/lab1_sched_types.h"
+#include <bits/stdc++.h>
+
+using namespace std;
